c36a93fbd34bb55efa1a51032963ce0d27e92fba
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-01-17 15:17:48 -0300

    feat: add error handling to signup function

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,10 +1,46 @@
 import createDataContext from "./createDataContext";
+import trackerApi from "../api/tracker";
 
 const authReducer = (state, action) => {
   switch (action.type) {
+    case "add_error":
+      return { ...state, errorMessage: action.payload };
     default:
       return state;
   }
 };
 
-export const { Provider, Context } = createDataContext(authReducer, {}, { isSignedIn: false });
+const signup = (dispatch) => {
+  return async ({ email, password }) => {
+    //make api request to sign up with that email and password
+    //  if we sign up, modify our state,and say that we are authenticated
+    //if signing up fails, we probably need to reflect an error message somewhere
+
+    try {
+      const response = await trackerApi.post("/signup", { email, password });
+      console.log(response.data);
+    } catch (err) {
+      dispatch({ type: "add_error", payload: "Something went wrong with signup" });
+    }
+  };
+};
+
+const signin = (dispatch) => {
+  return ({ email, password }) => {
+    //try to sign in
+    //handle success by updating state
+    // handle failure by showing error message (somehow)
+  };
+};
+
+const signout = (dispatch) => {
+  return () => {
+    //somehow sign out
+  };
+};
+
+export const { Provider, Context } = createDataContext(
+  authReducer,
+  { signup, signin, signout },
+  { isSignedIn: false, errorMessage: "" }
+);
