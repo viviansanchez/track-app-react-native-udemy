6abb97adc43f890507eb6cdfa9b03a1c4f02f4c8
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-01-17 13:17:45 -0300

    feat: create SignUp screen structure, styling and states

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
+
+const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>SignUp for Tracker</Text>
+      </Spacer>
+      <Input label="Email" value={email} onChangeText={setEmail} autoCapitalize="none" autoCorrect={false} />
+      <Spacer />
+      <Input
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
+      />
+      <Spacer>
+        <Button title="Sign Up" />
+      </Spacer>
+    </View>
+  );
+};
+
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 200,
+  },
+});
+
+export default SignupScreen;
