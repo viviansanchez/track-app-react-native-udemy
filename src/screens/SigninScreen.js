443b1a17fd811656fed1db7ec5043d72f051c7f5
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-01-17 17:26:50 -0300

    feat: implement AuthForm and NavLink

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, StyleSheet, Text } from "react-native";
+import AuthForm from "../components/AuthForm";
+import NavLink from "../components/NavLink";
+import { Context as AuthContext } from "../context/AuthContext";
 
 const SigninScreen = () => {
-  return <Text style={{ fontSize: 40 }}>SigninScreen</Text>;
+  const { state, signin } = useContext(AuthContext);
+
+  return (
+    <View style={styles.container}>
+      <AuthForm
+        errorMessage={state.errorMessage}
+        headerText="Sign In to Your Account"
+        submitButtonText="Sign In"
+        onSubmit={signin}
+      />
+      <NavLink routeName="Signup" text="Don't have an account? Sign up instead" />
+    </View>
+  );
 };
 
-const style = StyleSheet.create({});
+SigninScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 200,
+  },
+});
 
 export default SigninScreen;
