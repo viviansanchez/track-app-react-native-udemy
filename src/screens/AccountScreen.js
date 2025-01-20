8efff1d8aa0113aa58b9d9a000ba9931d94775ae
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-01-20 15:21:03 -0300

    feat: add AccountScreen jsx and functionality

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { View, StyleSheet, Text } from "react-native";
+import { Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
+import { Context as AuthContext } from "../context/AuthContext";
 
 const AccountScreen = () => {
-  return <Text style={{ fontSize: 40 }}>AccountScreen</Text>;
+  const { signout } = useContext(AuthContext);
+  return (
+    <>
+      <Text style={{ fontSize: 40 }}>AccountScreen</Text>
+      <Spacer>
+        <Button title="Signout" onPress={signout} />
+      </Spacer>
+    </>
+  );
 };
 
 const style = StyleSheet.create({});
