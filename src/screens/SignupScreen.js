af8417346496789e5b0badeabe99222a1b97e5b9
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-01-20 14:40:57 -0300

    feat: added useEffect to invoke tryLocalSignin function

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import { Context as AuthContext } from "../context/AuthContext";
 import AuthForm from "../components/AuthForm";
@@ -6,7 +6,11 @@ import NavLink from "../components/NavLink";
 import { NavigationEvents } from "react-navigation";
 
 const SignupScreen = ({ navigation }) => {
-  const { state, signup, clearErrorMessage } = useContext(AuthContext);
+  const { state, signup, clearErrorMessage, tryLocalSignin } = useContext(AuthContext);
+
+  useEffect(() => {
+    tryLocalSignin();
+  }, []);
 
   return (
     <View style={styles.container}>
