8982c065ad911ea90aae4ff2da3ff47cf173f9b5
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-01-20 15:31:28 -0300

    chore: added SafeAreaView component to prevent screen going into safe area

--- src/screens/AccountScreen.js
+++ src/screens/AccountScreen.js
@@ -3,16 +3,17 @@ import { View, StyleSheet, Text } from "react-native";
 import { Button } from "react-native-elements";
 import Spacer from "../components/Spacer";
 import { Context as AuthContext } from "../context/AuthContext";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const AccountScreen = () => {
   const { signout } = useContext(AuthContext);
   return (
-    <>
+    <SafeAreaView forceInset={{ top: "always" }}>
       <Text style={{ fontSize: 40 }}>AccountScreen</Text>
       <Spacer>
         <Button title="Signout" onPress={signout} />
       </Spacer>
-    </>
+    </SafeAreaView>
   );
 };
 
