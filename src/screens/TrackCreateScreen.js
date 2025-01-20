d0cd83f0897d170ecab5c22c8195553e8002118b
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-01-20 16:50:33 -0300

    feat: add startWatching function to monitor user location

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,14 +1,44 @@
-import React from "react";
+import "../_mockLocation";
+import React, { useEffect, useState } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
 import Map from "../components/Map";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { requestForegroundPermissionsAsync, watchPositionAsync, Accuracy } from "expo-location";
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync();
+      if (!granted) {
+        throw new Error("Location permission not granted");
+      }
+
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        (location) => {
+          console.log(location);
+        }
+      );
+    } catch (err) {
+      setErr(err);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <SafeAreaView forceInset={{ top: "always" }}>
       <Text h2> Create a Track</Text>
       <Map />
+      {err && <Text>Please enable location services</Text>}
     </SafeAreaView>
   );
 };
