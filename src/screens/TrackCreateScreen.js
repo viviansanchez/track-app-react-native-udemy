ba0ae48bda6a6ddc4833cd47a4226f76aa8599cf
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-01-20 16:00:42 -0300

    refactor: update jsx to show Map component, add safe area view

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { StyleSheet } from "react-native";
+import { Text } from "react-native-elements";
+import Map from "../components/Map";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const TrackCreateScreen = () => {
-  return <Text style={{ fontSize: 40 }}>TrackCreateScreen</Text>;
+  return (
+    <SafeAreaView forceInset={{ top: "always" }}>
+      <Text h2> Create a Track</Text>
+      <Map />
+    </SafeAreaView>
+  );
 };
 
 const style = StyleSheet.create({});
