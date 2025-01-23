37454e42da68e7e8f464cd74908df7dbbb52cb87
Vivian Sanchez <vivian.sanchez.dev@gmail.com>
2025-01-23 12:19:38 -0300

    chore: changed starting longitude and altitude

--- src/_mockLocation.js
+++ src/_mockLocation.js
@@ -11,8 +11,8 @@ const getLocation = (increment) => {
       accuracy: 5,
       altitudeAccuracy: 5,
       altitude: 5,
-      longitude: -122.0312186 + increment * tenMetersWithDegrees,
-      latitude: 37.33233141 + increment * tenMetersWithDegrees,
+      longitude: -46.724728164417726 + increment * tenMetersWithDegrees,
+      latitude: -23.579040039448003 + increment * tenMetersWithDegrees,
     },
   };
 };
